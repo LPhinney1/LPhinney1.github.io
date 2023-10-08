fbf6412f83851a2e671bb238430b6ae5fccead98
LPhinney1 <142834134+LPhinney1@users.noreply.github.com>
2023-10-08 13:25:26 -0400

    Added BYO_Intro page and scripts, some other small edits.

--- lphinne1/itis3135/scripts/defaultscript.js
+++ lphinne1/itis3135/scripts/defaultscript.js
@@ -3,8 +3,11 @@ function current_time()
     //Today is 3:53pm on Wednesday, 3 November, 2027
     var new_date = new Date();
     var new_time = 'Time is '+(new_date.getHours()%12)+':'+new_date.getMinutes()+':'+new_date.getSeconds()+' on '+(new_date.getMonth()+1)+'-'+new_date.getDate()+'-'+new_date.getFullYear();
-    document.getElementById("time").innerHTML = new_time;
+    document.getElementById("get_current_time").innerHTML = new_time;
 }
+
+setInterval(current_time, 1000); //activate function on one second interval.
+
 function greeting() 
 {
     var name = document.getElementById("name").value;
@@ -54,27 +57,27 @@ function favorite_number()
         }
     }
 }
-function f1()
+function button1()
 {  
     var renameThis = prompt('What text would you like to add to the webpage?','Enter your message here.');
     var element = document.getElementById("function2_target");
     if (renameThis != "Enter your message here.")
     { element.textContent = renameThis; }
 }
-function f2() 
+function button2() 
 {
     var hexCode = "#" + Math.floor(Math.random()*16777215).toString(16);
     var element = document.getElementById("function1_target");
     element.style.backgroundColor = hexCode;
 }
-function f3() 
+function button3() 
 {
     var principal = prompt('Enter Principal Amount as Whole Number','(ex. 20000)');
     var rate = prompt('Enter Interest Rate as Decimal','(ex. 0.03 = 3%)');
     var time = prompt('Enter the Length in Years','(ex. 5, 10)');
     alert('The amount of interest you will pay is $'+Math.round((principal*rate*time)*100)/100);
 }
-function f4() 
+function button4() 
 {
     var randomNumber = Math.floor(Math.random() * 76) + 1;
     switch (randomNumber) {
@@ -310,4 +313,3 @@ function f4()
             break;
     }
 }
-setInterval(current_time, 1000);
---
+++ lphinne1/itis3135/scripts/survey.js
@@ -0,0 +1,144 @@
+//ON SUBMIT BUTTON LISTENER
+document.getElementById("main-form").addEventListener("submit", function(_afterSubmit) 
+{
+    event.preventDefault();
+
+    //TITLE / MASCOT
+    let name = document.getElementById("name").value;
+    let mascot = document.getElementById("mascot").value;
+    var title = document.createElement('h1');
+    title.textContent = name +"'s "+ mascot;
+    document.getElementById('main-form').appendChild(title);
+    var introText = document.createElement("h2");
+    introText.textContent = "Introduction";
+    document.getElementById('main-form').appendChild(introText);
+
+
+    //IMAGE / FIGURE
+    var figureImage = document.createElement('figure');
+    figureImage.id = "figureImage"
+    document.getElementById('main-form').appendChild(figureImage);
+
+    let image = document.getElementById("imageUp");
+    var imageFile = image.files[0];
+    if (imageFile) 
+    {
+        var imageResult = document.createElement('img');
+        imageResult.src = URL.createObjectURL(imageFile);
+        imageResult.className = "image-intro";
+        document.getElementById('figureImage').appendChild(imageResult);
+        URL.revokeObjectURL(imageResult);
+    }
+
+    //IMAGE CAPTION
+    let caption = document.getElementById("caption").value;
+    var captionText = document.createElement('figcaption');
+    captionText.textContent = caption;
+    document.getElementById('figureImage').appendChild(captionText);
+
+    //LIST / UL ELEMENT
+    var mainList = document.createElement('ul');
+    mainList.id = "main-list"
+    document.getElementById('main-form').appendChild(mainList);
+
+
+    //PERSONAL BG
+    let personal_background = document.getElementById("personal_background").value;
+    var personal_backgroundText = document.createElement('li');
+    personal_backgroundText.textContent = 'Personal Background: '+personal_background;
+    document.getElementById('main-list').appendChild(personal_backgroundText);
+
+    //PROFESSIONAL BG
+    let professional_background = document.getElementById("professional_background").value;
+    var professional_backgroundText = document.createElement('li');
+    professional_backgroundText.textContent = 'Professional Background: '+professional_background;
+    document.getElementById('main-list').appendChild(professional_backgroundText);
+
+    //ACADEMIC BG
+    let academic_background = document.getElementById("academic_background").value;
+    var academic_backgroundText = document.createElement('li');
+    academic_backgroundText.textContent = 'Academic Background: '+academic_background;
+    document.getElementById('main-list').appendChild(academic_backgroundText);
+
+    //WEB DEV BG
+    let web_dev_background = document.getElementById("web_dev_background").value;
+    var web_dev_backgroundText = document.createElement('li');
+    web_dev_backgroundText.textContent = 'Background in this Subject: '+web_dev_background;
+    document.getElementById('main-list').appendChild( web_dev_backgroundText);
+
+    //PLATFORM
+    let platform = document.getElementById("platform").value;
+    var platformText = document.createElement('li');
+    platformText.textContent = 'Primary Computer Platform: ' + platform;
+    document.getElementById('main-list').appendChild(platformText);
+
+    //CLASSES
+    var dividerUL = document.createElement('ul');    
+    dividerUL.textContent = "Classes I'm Taking: ";    
+    document.getElementById('main-list').appendChild(dividerUL);
+    var classesText = document.createElement('ul');
+    classesText.id = 'classesList';
+    document.getElementById('main-list').appendChild(classesText);
+
+    var classesInputs = document.querySelectorAll('input#classesText');
+    classesInputs.forEach((input) => {
+        let classes = input.value;
+        var classesText2 = document.createElement('li');
+        classesText2.textContent = classes;
+        classesText.appendChild(classesText2);
+        document.getElementById('classesList').appendChild(classesText2);
+    });
+
+    //OPTIONAL
+    if (document.getElementById("funny").value !== '')
+    {
+        let funny = document.getElementById("funny").value;
+        var funnyText = document.createElement('li');
+        funnyText.textContent = funny;
+        document.getElementById('main-list').appendChild(funnyText);
+    }
+
+    if (document.getElementById("additional").value !== '')
+    {
+        let additional = document.getElementById("additional").value;
+        var additionalText = document.createElement('li');
+        additionalText.textContent = "I'd also like to Share: "+additional;
+        document.getElementById('main-list').appendChild(additionalText);
+    }
+});
+
+//ON RESET BUTTON LISTENER
+document.getElementById("main-form").addEventListener("reset", function(_afterReset) 
+{
+    window.location.reload();
+});
+
+//ADD TEXT BOX
+function addTextEntry()
+{
+    var newLabel = document.createElement('label');
+    newLabel.id = 'classes';
+    newLabel.htmlFor = 'classesText'
+    newLabel.textContent = '(Class ID#) + | + (Course Description) | ';
+    
+    var newText = document.createElement('input');    
+    newText.type = 'text';
+    newText.id = 'classesText';
+    newText.name = 'classesText';
+    newText.required = true;
+
+    newLabel.appendChild(newText);
+    document.getElementById('classesTextEntry').appendChild(newLabel);
+}
+
+//REMOVE TEXT BOX
+function deleteTextEntry()
+{
+
+    document.getElementById("classes").remove();        
+    if(document.getElementById("classes") === null)
+    {
+        alert("You have to add at least one class.");
+        addTextEntry();
+    }
+}

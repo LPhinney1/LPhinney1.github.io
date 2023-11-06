8393ff14dcb55c220eeb9754f3cf6268aece7038
LPhinney1 <142834134+LPhinney1@users.noreply.github.com>
2023-11-05 20:34:29 -0500

    Added Client Project: BLNC

---
+++ lphinne1/itis3135/blnc_client_project/scripts/defaultscript.js
@@ -0,0 +1,17 @@
+const galleryContainer = document.body;
+galleryContainer.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target.tagName === 'IMG' && target.parentElement.tagName === 'FIGURE') {
+        const figure = target.parentElement;
+        const figcaption = figure.querySelector('figcaption');
+        if (figcaption.style.display === 'none') { figcaption.style.display = 'block'; }
+        else { figcaption.style.display = 'none'; }
+    }
+
+    if (target.tagName === 'H2' && target.parentElement.tagName === 'DIV') {
+        const div = target.parentElement;
+        const paragraph = div.querySelector('p');
+        if (paragraph.style.display === 'none') { paragraph.style.display = 'block'; }
+        else { paragraph.style.display = 'none'; }
+    }
+});
